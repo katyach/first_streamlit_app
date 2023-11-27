941d3054c6f12734111a1f4249659296a63c5641
Cathy <eyc1@ualberta.ca>
2023-11-26 22:38:41 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,5 +61,6 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 #does not show choice, still. need to add the table somehow
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) 
-streamlit.dataframe(fruityvice_normalized)
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) DID NOT WORK
+
+streamlit.dataframe(fruityvice_normalized) #STILL OLD TABLE
